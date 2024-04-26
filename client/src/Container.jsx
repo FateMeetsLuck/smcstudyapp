8b270b15bf1065297679b2f742157ef3b10c888c
FateMeetsLuck <grindstaff0john@gmail.com>
2024-04-25 20:07:13 -0400

    Cleanup and organization to begin REST API implementation

--- client/src/Container.jsx
+++ client/src/Container.jsx
@@ -2,13 +2,13 @@
  * A container block element that can hold one paragraph passage in the left column, and notes on the right
  */
 import PropTypes from "prop-types"
-import Paragraph from "./Paragraph"
+import BookNavigation from "./BookNavigation"
 import Annotation from "./Annotation"
 
 function Container(props) {
     return(
         <div class="container">
-            <Paragraph referenceID={props.refID} text={props.text} />
+            <BookNavigation referenceID={props.refID} text={props.text} />
             <Annotation content={props.note} />
         </div>
     );
